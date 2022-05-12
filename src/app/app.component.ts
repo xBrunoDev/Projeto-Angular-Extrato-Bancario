91f6fdd6d17f0b6b8d825af11be84c17a5651cce
xBrunoDev <bruno_gabriel06@hotmail.com>
2022-05-12 14:57:10 -0300

    Iniciando projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencias: any[] = [];
+
+  transferir($event){
+    console.log($event);
+    const transferencia = {...$event, data: new Date()};
+    this.transferencias.push(transferencia);
+  }
+
 }
